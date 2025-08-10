3630f3da929058ae16a5a0c886f8676b93906521
Roberta França <lucasyohanfmarinho@gmail.com>
2025-08-10 16:45:10 -0300

    remocao chaves

--- api/index.js
+++
@@ -1,7 +0,0 @@
-import md5 from "md5";
-
-const ts = 1;
-const publicKey = "f1e374b5827ccfbc4032c97db46bd8b8";
-const privateKey = "5850686947501e198f9dea099e828434b89fe1e1";
-
-console.log(md5(ts + privateKey + publicKey));
